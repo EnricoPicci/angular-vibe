9ec8b7bdccfd5797318b3f3b85c8cf708c335e25
Picci <enrico.piccinin@capgemini.com>
2025-05-14 11:42:52 +0200

    routing added

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,19 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { ApplicationConfig } from '@angular/core';
 import { provideHttpClient } from '@angular/common/http';
+import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { AppComponent } from './app/app.component';
+import { HomeComponent } from './app/pages/home/home.component';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideHttpClient(),
-    // ...other providers
+    provideRouter(
+      [
+        { path: '', component: HomeComponent, pathMatch: 'full' }
+      ],
+      withComponentInputBinding()
+    ),
   ]
 };
 
