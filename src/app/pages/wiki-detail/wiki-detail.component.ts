45d86e447f27dddfeb4b77fa23e8fe49f8106e14
Picci <enrico.piccinin@capgemini.com>
2025-05-14 12:43:29 +0200

    wiki detail component created

---
+++ src/app/pages/wiki-detail/wiki-detail.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-wiki-detail',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './wiki-detail.component.html',
+  styleUrl: './wiki-detail.component.css'
+})
+export class WikiDetailComponent {}
